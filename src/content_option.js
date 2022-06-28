eda2d265ad6e371c07bc579fc58d4f090cd2a72a
alidhanani <alidhanani@hotmail.com>
2022-06-28 16:41:54 +0200

    first view

---
+++ src/content_option.js
@@ -0,0 +1,199 @@
+const logotext = "ALI DHANANI";
+const meta = {
+    title: "Ali Dhanani",
+    description: "I’m Ali Dhanani software engineer, currently working in Brussels",
+};
+
+const introdata = {
+    title: "I’m Ali Dhanani",
+    animated: {
+        first: "I'm an application developer",
+        second: "An application security expert",
+        third: "A car enthusiast",
+    },
+    description: "Development of software with up to date security aspect for client satisfaction",
+    your_img_url: "/MyPic.jpg",
+};
+
+const dataabout = {
+    title: "Who is Ali Dhanani?",
+    aboutme: "A knowledgeable young person who is able to provide Application Frontend and Backend Development Support as part of a Software Developing Service. I am flexible, resourceful, and willing to take full ownership of user problems until they are fully resolved.",
+};
+const worktimeline = [{
+        jobtitle: "Software Engineer",
+        where: "nextAuth",
+        date: "02/2021 - Present",
+        place: "Brussels, Belgium",
+    },
+    {
+        jobtitle: "Trainee Software Engineer",
+        where: "Sirris",
+        date: "07/2020 - 06/2021",
+        place: "Brussels, Belgium",
+    },
+    {
+        jobtitle: "Associate iOS Developer",
+        where: "Livewire Labs",
+        date: "05/2019 - 08/2019",
+        place: "Karachi, Pakistan",
+    },
+    {
+        jobtitle: "Software Developer",
+        where: "Nerdiacs Pte",
+        date: "09/2017 - 04/2019",
+        place: "Karachi, Pakistan",
+    },
+];
+
+const skills = [{
+        name: "Swift",
+        value: 80,
+    },
+    {
+        name: "Objective-C",
+        value: 50,
+    },
+    {
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "React Native (JS, TS)",
+        value: 70,
+    },
+    {
+        name: "Go",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const education = [{
+    title: "Master in Computer Science",
+    institue: "Universite Libre De Bruxelles",
+    year: "2019 to 2021",
+    score: "Satisfactory",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Bachelors in Computer Science",
+    institue: "Shaheed Zulfikar Ali Bhutto Institute Of Science And Technology",
+    year: "2014 to 2018",
+    score: "3.24/4.00",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Computer Security",
+    institue: "SecAppDev (Secure Application Development)",
+    year: "2022",
+    score: "1.66 ECT",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "alidhanani@hotmail.com",
+    YOUR_FONE: "+32-494-46-3188",
+    description: "Send me a message and I will try my best to respond as quick as possible",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/alidhanani",
+    facebook: "https://www.facebook.com/ali.dhanani.61819/",
+    linkedin: "https://www.linkedin.com/in/alidhanani61819/",
+    twitter: "https://twitter.com/AliDhanani95",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    education,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
